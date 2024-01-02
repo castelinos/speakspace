dba2ebf0761ef07fae92ce69987537f7ae0052a8
castelinos <sheltcas10@gmail.com>
2024-01-02 13:07:35 +0530

    Setup onboarding form for registration

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,25 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+
+import '../globals.css';
+
+export const metadata = {
+    title: 'Speakspace',
+    description: 'Nextjs 13 social media application'
+}
+
+const inter = Inter({ subsets: ['latin'] });
+
+function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <ClerkProvider>
+            <html lang="en">
+                <body className={`${inter.className} bg-dark-1`}>
+                    {children}
+                </body>
+            </html>
+        </ClerkProvider>
+    )
+}
+
+export default RootLayout;
