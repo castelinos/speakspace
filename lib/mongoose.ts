b73f6a6a2902d87d5131788d36677410fcf5ce3c
castelinos <sheltcas10@gmail.com>
2024-01-17 18:38:28 +0530

    configure connection to mongodb

---
+++ lib/mongoose.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+let isConnected = false;
+
+export const connectToDb = async () => {
+    mongoose.set( 'strictQuery', true );
+
+    if( !process.env.MONGODB_URL ) {
+        console.log('MongoDB URL not found!!!');
+        return;
+    }
+
+    if( isConnected ) {
+        console.log('Connection to MongoDB exists');
+        return;
+    }
+
+    try {
+        let c = await mongoose.connect( process.env.MONGODB_URL );
+        isConnected = true;
+
+        console.log('Connected to MongoDB');
+        
+        
+    } catch (error) {
+        console.log('Error connecting MongoDB',error);
+    }
+}
