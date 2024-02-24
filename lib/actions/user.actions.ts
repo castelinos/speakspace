93295c1f4215c402851dab982b2e881b44827e82
castelinos <sheltcas10@gmail.com>
2024-02-24 23:20:56 +0530

    Created user profile page

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,10 +1,11 @@
 "use server";
 
-import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
+import Thread from "../models/thread.model";
 import { connectToDb } from "../mongoose";
+import { revalidatePath } from "next/cache";
 
-interface Params {
+interface updateUserParams {
   userId: string;
   username: string;
   name: string;
@@ -20,7 +21,7 @@ export async function updateUser({
   bio,
   image,
   path,
-}: Params): Promise<void> {
+}: updateUserParams): Promise<void> {
   await connectToDb();
 
   try {
@@ -51,8 +52,38 @@ export async function updateUser({
 export async function fetchUser(userId: string) {
   try {
     connectToDb();
-    return await User.findOne({ id: userId });
+
+    const user = await User.findOne({ id: userId });
+    return user;
+
   } catch (error: any) {
     console.log("Failed to fetch user by ID", error.message);
+    return [];
+  }
+}
+
+export async function fetchUserPosts(userId: string) {
+  try {
+    connectToDb();
+
+    const threads = await User.findOne({ id: userId }).populate({
+      path: "threads",
+      model: Thread,
+      populate: {
+        path: "children",
+        model: Thread,
+        populate: {
+          path: "author",
+          model: User,
+          select: "name image id",
+        },
+      },
+    });
+
+    return threads;
+
+  } catch (error: any) {
+    console.log('Failed to fetch user posts', error.message);
+    return [];
   }
 }
